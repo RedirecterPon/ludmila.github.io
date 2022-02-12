2532bcc38030bb4b1783e89e4f999bff95dc5875
RedirecterPon <99540343+RedirecterPon@users.noreply.github.com>
2022-02-12 16:03:55 +0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
-        window.location.replace("https://www1.lone1y.com/click?pid=50979&offer_id=25")
+        let click = document.querySelector('.click')
+        // click.click()
     }, 400)
 });
