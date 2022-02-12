2bc71c0c772add734e030b5ad7481c0ab3f2c464
RedirecterPon <99540343+RedirecterPon@users.noreply.github.com>
2022-02-12 15:47:54 +0300

    .

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        window.location.replace("https://www1.lone1y.com/click?pid=50979&offer_id=25")
+    }, 400)
+});
